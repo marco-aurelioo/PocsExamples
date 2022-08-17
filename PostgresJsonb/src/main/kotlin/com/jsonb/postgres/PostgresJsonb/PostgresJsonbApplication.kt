90edaf6e1875adbf158f799d026f7dcbf6567021
marco-aurelioo <marco_aurelioo@yahoo.com.br>
2022-08-16 22:34:06 -0300

    initial commit

---
+++ PostgresJsonb/src/main/kotlin/com/jsonb/postgres/PostgresJsonb/PostgresJsonbApplication.kt
@@ -0,0 +1,11 @@
+package com.jsonb.postgres.PostgresJsonb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostgresJsonbApplication
+
+fun main(args: Array<String>) {
+	runApplication<PostgresJsonbApplication>(*args)
+}
---
+++ PostgresJsonb/src/test/kotlin/com/jsonb/postgres/PostgresJsonb/PostgresJsonbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jsonb.postgres.PostgresJsonb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostgresJsonbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
